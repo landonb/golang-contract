b6d8c524554f3b64abc6c0138402a88bf0b5573a
Landon Bouma <landon.bouma@gmail.com>
2016-10-28 12:04:49 -0500

    Add contract.

---
+++ contract.go
@@ -0,0 +1,12 @@
+// File: contract.go
+// Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
+// Last Modified: 2016.10.28
+// Project Page: https://github.com/landonb/golang_contract
+// Summary: A design by contract `assert` mechanism for devs.
+// License: GPLv3
+
+package contract
+
+
+
+
