1795010f036f318febf0bd2cabf647c0d1354b73
Landon Bouma <landon.bouma@gmail.com>
2016-10-29 17:15:28 -0500

    Fix logic.

--- contract.go
+++ contract.go
@@ -1,6 +1,6 @@
 // File: contract.go
 // Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-// Last Modified: 2016.10.28
+// Last Modified: 2016.10.29
 // Project Page: https://github.com/landonb/golang_contract
 // Summary: A design by contract `assert` mechanism for devs.
 // License: GPLv3
@@ -15,12 +15,12 @@ import (
 var LOG = log.New(os.Stdout, "[third-wish] ", log.Ldate|log.Lmicroseconds|log.LUTC)
 
 func Contract(condition bool) {
-	if condition {
+	if !condition {
 		LOG.Printf("contract failure: %+v", condition)
 // FIXME: Introspect and print file/line of cbdfail.
 // FIXME: dlv connect --init="set_breakpoints.dlv" localhost:3001
 //          where set_breakpoints.dlv is
-//          break contract.go:16
+//          break contract.go:19
 	}
 }
 
