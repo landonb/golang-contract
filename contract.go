83ae4fa7e1a2cad73031675f1cd51af9c108eeb5
Landon Bouma <landon.bouma@gmail.com>
2016-10-31 14:14:19 -0500

    Mistakes were made.

--- contract.go
+++ contract.go
@@ -1,9 +1,9 @@
 // File: contract.go
 // Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-// Last Modified: 2016.10.29
+// Last Modified: 2016.10.31
 // Project Page: https://github.com/landonb/golang_contract
 // Summary: A design by contract `assert` mechanism for devs.
-// License: GPLv3
+// License: Apache 2.0 (See file: LICENSE).
 
 package contract
 
